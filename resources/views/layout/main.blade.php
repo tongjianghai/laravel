cf560e71dd1a33eaa364bc9b20125e0ad4c784ab
maolei <2889598553@qq.com>
2022-03-07 11:56:10 +0800

    bootcdn

--- resources/views/layout/main.blade.php
+++ resources/views/layout/main.blade.php
@@ -52,8 +52,8 @@
     <!-- Bootstrap core JavaScript
 ================================================== -->
     <!-- Placed at the end of the document so the pages load faster -->
-    <script src="https://cdn.bootcss.com/jquery/1.12.4/jquery.min.js"></script>
-    <script src="https://cdn.bootcss.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    <script src="https://cdn.bootcdn.net/ajax/libs/jquery/1.12.4/jquery.js"></script>
+    <script src="https://cdn.bootcdn.net/ajax/libs/twitter-bootstrap/3.3.7/js/bootstrap.js"></script>
     <script type="text/javascript" src="/js/wangEditor.min.js"></script>
     <script src="/js/ylaravel.js"></script>
 
